1cd5cf9f2070ce9924b5997d8c582492a9e5c791
jdespinosa <espinosa_jd1@yahoo.com>
2018-03-23 15:27:30 +0800

    Create numcomm.js

---
+++ numcomm.js
@@ -0,0 +1,18 @@
+function formatVal(val)
+{
+	// split value to whole and decimal comps
+	var wVal = val.split('.')[0];
+	var dVal = ( val.indexOf('.') == -1 ? '' : val.split('.')[1]);
+	
+	// format whole no. comp
+	return vBuff + ( dVal != '' ? '.' + dVal : '' );
+	var dCount = 0;
+	for(var i=wVal.length-1; i>=0; i--){
+		vBuff = wVal.charAt(i) + vBuff;
+		dCount++;
+		if( dCount == 3 && i > 0 ){
+			vBuff = "," + vBuff;
+			dCount = 0;
+		}
+	}
+}
